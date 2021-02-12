5289b9b92643275f52a47d4a3f3f51bc2ce166c0
Alina Zhang <alina.li.zhang@gmail.com>
2021-02-12 16:57:09 -0500

    Update MergeSortCountingInversions.py

--- MergeSortCountingInversions.py
+++ MergeSortCountingInversions.py
@@ -22,18 +22,16 @@ def countInversions(arr):
     arr2 = arr[n1:]
     results = countInversions(arr1) + countInversions(arr2)
     
-    # 2 1 3 1 2
-    # n1=2, n2=3
-    # arr1 = [2,1], arr2 = [3,1,2]
-    # [2,1]
+    # [1,2]
     # n1=1, n2=1
-    # arr1 = [2], arr2 = [1]
+    # arr1 = [1], arr2 = [2]
     # merge arr1 and arr2
     i1 = 0
     i2 = 0
     for i in range(n):
         if i1 <n1 and (i2>=n2 or arr1[i1]<=arr2[i2]):
             arr[i] = arr1[i1]
+            # i2 could be equal to 0
             results += i2
             i1 += 1 
         elif i2 < n2:
